2009eae47ad39784c1348dfd13b5707e8616ca23
Leonard Karoly <leonard.a.karoly@gmail.com>
2022-11-30 14:15:42 -0600

    Array Introduction commit

---
+++ warm_exercise.js
@@ -0,0 +1,10 @@
+/*PB&J Warm up
+walk over to the pantry
+open the pantry
+grab the peanut butter and bread
+place peanut butter and bread on kitchen counter
+---
+go to the fridge
+open the fridge
+grab the jelly
+ */

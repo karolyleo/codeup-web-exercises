723f746ab86ec398406ea6c269f134eece353600
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-01-24 17:58:51 -0600

    Completed

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,8 +1,8 @@
 "use strict"
-let latLong = [-98.4916, 29.4252];
+let latLong = [-98.4916, 29.4252]; //default is San Antonio
 
 mapboxgl.accessToken = keys.mapbox;// easier access to my key
-const coordinates = document.getElementById('coordinates');
+const coordinates = document.getElementById('coordinates'); //or $('#coordinates')
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/karolleo000/cld93zwc1001r01oe4192n12v',
@@ -10,10 +10,9 @@ let map = new mapboxgl.Map({
     center: latLong //starting position in San Antonio
 })
 
-//Testing Map Stuff
 const canvas = map.getCanvasContainer();
 
-const geojson = {
+let geojson = {
     'type': 'FeatureCollection',
     'features': [
         {
@@ -25,33 +24,6 @@ const geojson = {
         }
     ]
 };
-
-function onMove(e) {
-    const coords = e.lngLat;
-
-// Set a UI indicator for dragging.
-    canvas.style.cursor = 'grabbing';
-
-// Update the Point feature in `geojson` coordinates
-// and call setData to the source layer `point` on it.
-    geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
-    map.getSource('point').setData(geojson);
-}
-
-function onUp(e) {
-    const coords = e.lngLat;
-
-// Print the coordinates of where the point had
-// finished being dragged to on the map.
-    coordinates.style.display = 'block';
-    coordinates.innerHTML = `Longitude: ${coords.lng}<br />Latitude: ${coords.lat}`;
-    canvas.style.cursor = '';
-    console.log(coords);
-
-// Unbind mouse/touch events
-    map.off('mousemove', onMove);
-    map.off('touchmove', onMove);
-}
 
 map.on('load', () => {
 // Add a single point to the map.
@@ -70,15 +42,14 @@ map.on('load', () => {
         }
     });
 
-// When the cursor enters a feature in
-// the point layer, prepare for dragging.
+// When the cursor enters a feature in the point layer, prepare for dragging.
     map.on('mouseenter', 'point', () => {
-        map.setPaintProperty('point', 'circle-color', '#3bb2d0');
+        map.setPaintProperty('point', 'circle-color', '#6ac12e');
         canvas.style.cursor = 'move';
     });
 
     map.on('mouseleave', 'point', () => {
-        map.setPaintProperty('point', 'circle-color', '#3887be');
+        map.setPaintProperty('point', 'circle-color', '#3bb2d0');
         canvas.style.cursor = '';
     });
 
@@ -97,13 +68,11 @@ map.on('load', () => {
 
 // Prevent the default map drag behavior.
         e.preventDefault();
-
         map.on('touchmove', onMove);
         map.once('touchend', onUp);
     });
 });
-//End of Map Stuff
-let helper = '';
+
 let weatherData = {
     updatePage : function (city) {
         $.get("http://api.openweathermap.org/data/2.5/weather", {
@@ -111,16 +80,47 @@ let weatherData = {
             q: city,
             units: "imperial"
         }).done(function (data) {
-            latLong = [data.coord.lon, data.coord.lat]
+            latLong = [data.coord.lon, data.coord.lat];
+            geojson.features[0].geometry.coordinates = latLong;
             updateToday(data);
+
             map.flyTo({
                 center: data.coord,
-                essential: true // this animation is considered essential with respect to prefers-reduced-motion
-            });
+                essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+                zoom: 10
+            })
+                .getSource('point').setData(geojson);
         });
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: keys.openWeather,
             q: city,
+            units: "imperial"
+        }).done(function(data) {
+            updateWeekly(data.list);
+        });
+    },
+    updatePageCoord : function (coord) {
+        //for today's weather
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: keys.openWeather,
+            lon: coord.lng.toFixed(3),
+            lat: coord.lat.toFixed(3),
+            units: "imperial"
+        }).done(function (data) {
+            latLong = [data.coord.lon, data.coord.lat];
+            updateToday(data);
+            //For repositioning
+            map.flyTo({
+                center: data.coord,
+                essential: true // this animation is considered essential with respect to prefers-reduced-motion
+            });
+        })// .catch(error => console.log(error));
+
+        //For weekly Forecast
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: keys.openWeather,
+            lon: coord.lng.toFixed(3),
+            lat: coord.lat.toFixed(3),
             units: "imperial"
         }).done(function(data) {
             updateWeekly(data.list);
@@ -143,12 +143,13 @@ $('#citySearch').on('keypress', function(e){
 $('#mapBtn').click(function(){
     $('#weatherBoxes').toggleClass('flex-column');
     $('#mapDiv').toggleClass('visually-hidden');
+    map.resize();
 });
 
 //This is to update the first card inputs
 function updateToday(info){
     let currentTemp = info.main.temp.toFixed(0),
-        temp = info.main.temp_min + '°F/' + info.main.temp_max + '°F';
+        temp = info.main.temp_min + '°F / ' + info.main.temp_max + '°F';
 
     $('h2').children('strong').html(currentTemp + '°F');
     $('#locationName').html(info.name);
@@ -157,8 +158,9 @@ function updateToday(info){
           <img id="mainIcon" src="https://openweathermap.org/img/wn/${info.weather[0].icon}.png" alt="" class="icon" />
           <p class="mb-0 text-xs text-muted">${info.weather[0].description}</p>
           <p class="mb-0 text-xs"><strong>${temp}</strong></p>
-          <p class="mb-0 text-xs text-muted">wind /  pressure</p>
-          <p class="mb-0 text-xs"><strong>${info.wind.speed + '  /  '+ info.main.pressure}</strong></p>`
+          <p class="mb-0 text-xs text-muted">Wind(mph) /  Pressure(hPa)</p>
+          <p class="mb-0 text-xs"><strong>${info.wind.speed + '  /  '+ info.main.pressure}</strong></p>
+            <p class="mb-0 text-xs text-muted">Humidity: <strong class="small">${info.main.humidity}%</strong></p>`
     )
     // console.log(currentTemp, info.name); //.main.temp for current temp & .name for location
 }
@@ -177,7 +179,7 @@ function updateWeekly(weeklyInfo){
           <p class="mb-0 text-xs">${weeklyInfo[i].weather[0].main} <img src="https://openweathermap.org/img/wn/${weeklyInfo[i].weather[0].icon}.png" alt="" class="icon" /></p>
           <p class="mb-0 text-xs text-muted">${weeklyInfo[i].weather[0].description}</p>
           <p class="mb-0 text-xs"><strong>${temp}</strong></p>
-          <p class="mb-0 text-xs text-muted">Wind /  Pressure</p>
+          <p class="mb-0 text-xs text-muted">Wind(mph) /  Pressure(hPa)</p>
           <p class="mb-0 text-xs"><strong>${weeklyInfo[i].wind.speed + '  /  '+ weeklyInfo[i].main.pressure}</strong></p>
           <p class="mb-0 text-xs text-muted">Humidity: <strong class="small">${weeklyInfo[i].main.humidity}%</strong></p>
         </div>`)
@@ -199,4 +201,37 @@ function getDayAbbr(date) {
     let options = { timeZone: 'UTC', weekday: 'short'};
     let day = new Intl.DateTimeFormat('en-US', options).format(dateObject);
     return daysAbbr[day];
+}
+
+//interactive pin on map functions
+function onMove(e) {
+    let coords = e.lngLat;
+
+// Set a UI indicator for dragging.
+    canvas.style.cursor = 'grabbing';
+
+// Update the Point feature in `geojson` coordinates
+// and call setData to the source layer `point` on it.
+    geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
+    map.getSource('point').setData(geojson);
+}
+function onUp(e) {
+    let coords = e.lngLat;
+
+// Print the coordinates of where the point had
+// finished being dragged to on the map.
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${coords.lng}<br />Latitude: ${coords.lat}`;
+    canvas.style.cursor = '';
+    map.flyTo({
+        center: coords,
+        essential: true // this animation is considered essential with respect to prefers-reduced-motion
+    });
+    weatherData.updatePageCoord(coords);
+    // helper = reverseGeocode(coords,keys.mapbox)
+    // console.log(helper, coords);
+
+// Unbind mouse/touch events
+    map.off('mousemove', onMove);
+    map.off('touchmove', onMove);
 }
